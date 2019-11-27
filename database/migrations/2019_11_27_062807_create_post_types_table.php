c90830568869ce6b8649639db4fde1c1687b1839
David L. Crush <david.l.crush@disney.com>
2019-11-27 11:34:29 -0500

    adding tables

--- database/migrations/2019_11_27_062807_create_post_types_table.php
+++ database/migrations/2019_11_27_062807_create_post_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('post_types', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('slug', 50)->unique();
             $table->string('name');
-            $table->string('username')->unique();
-            $table->string('display_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('post_types');
     }
 }
